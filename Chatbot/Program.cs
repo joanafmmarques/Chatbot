36346ff807f0971378245f4f60c943b5152d0b93
Joana Marques <joana.pipaa@gmail.com>
2018-03-01 16:37:26 +0000

    Add a project: Talk to the computer

---
+++ Chatbot/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Aula03
+{
+    class Chatbot
+    {
+        static void Main(string[] args)
+        {
+            string question, answer;
+
+
+            do
+            {
+                Console.Write("Coloque uma questão: ");
+                question = Console.ReadLine();
+
+                if (question == "EXIT")
+                {
+                    break;
+                }
+                else
+                {
+                    switch (question)
+                    {
+                        case "How are you?":
+                            answer = "I'm fine, and you?";
+                            break;
+
+                        case "Hello, what's your name?":
+                            answer = "Hi, my name is Rob.";
+                            break;
+
+                        case "Do you like bananas?":
+                            answer = "NO!";
+                            break;
+
+                        case "Tell me a joke":
+                            answer = "Knock, knock. " +
+                                "Who’s there? Adore. " +
+                                "Adore who? " +
+                                "Adore is between us. Open up!";
+                            break;
+
+                        default:
+                            answer = "I don't know how to answer that.";
+                            break;
+                    }
+                    Console.WriteLine(answer);
+                }
+            }
+                while (question != "EXIT") ;
+            
+        }
+    }
+}
